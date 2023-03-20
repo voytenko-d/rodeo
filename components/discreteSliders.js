02d0971b998744debd450217501f72ac8084f731
Dmitry Voitenko <voytenko.d@ideasoft.io>
2023-03-20 18:13:23 +0000

    feat/rod-7 Fix leverage slider
    - fix getting input ref
    - fix dependency for useCallback

--- components/discreteSliders.js
+++ components/discreteSliders.js
@@ -1,7 +1,7 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useState } from "react";
 
 export default function DiscreteSliders({ min, max, step = "0.1", range = 5, value, onInput, className = "" }) {
-  const trackRef = useRef();
+  const [trackRef, setTrackRef] = useState({});
 
   const pv = (x, track) => {
     const r = (max - min) / (range - 1);
@@ -30,16 +30,19 @@ export default function DiscreteSliders({ min, max, step = "0.1", range = 5, val
               onInput={(e) => onInput(e.target.value)}
               key={i}
               value={v}
-              className={`discrete-sliders__delimiters-element ${value >= v ? 'delimiter-active' : ''}`}
+              className={`discrete-sliders__delimiters-element ${value >= v && min !== max ? 'delimiter-active' : ''}`}
               style={{ left: `${p}px` }}
             ></span>
           );
         })}
-        <span className={`discrete-sliders__delimiters-element ${value >= max ? 'delimiter-active' : ''}`} value={max}
-              style={{ left: `${track}px` }}></span>
+        <span
+          className={`discrete-sliders__delimiters-element ${value >= max && min !== max ? 'delimiter-active' : ''}`}
+          value={max}
+          style={{ left: `${track}px` }}
+        ></span>
       </div>
-    )
-  }, [trackRef.current, value]);
+        )
+    }, [trackRef.current, value, min, max]);
 
   const TrackWithMarkers = useCallback(() => {
     const track = trackRef.current?.offsetWidth;
@@ -49,17 +52,20 @@ export default function DiscreteSliders({ min, max, step = "0.1", range = 5, val
     return (
       <div className="discrete-sliders__markers" style={{ width: `${track}px` }}>
         <span className="discrete-sliders__markers-element" value={min}
-              style={{ left: `0px` }}>{min.toFixed(1) + 'x'}</span>
+          style={{ left: `0px` }}
+        >{min.toFixed(1) + 'x'}</span>
         {Array.from([...Array(range - 2).keys()], (x, i) => {
           const { p, v } = pv(x, track);
-          return <span className="discrete-sliders__markers-element" key={i} value={v}
-                       style={{ left: `${p}px` }}>{v.toFixed(1) + 'x'}</span>
+          return (
+            <span className="discrete-sliders__markers-element" key={i} value={v} style={{ left: `${p}px` }}>{v.toFixed(1) + 'x'}</span>
+          );
         })}
         <span className="discrete-sliders__markers-element" value={max}
-              style={{ left: `${track}px` }}>{max.toFixed(1) + 'x'}</span>
+          style={{ left: `${track}px` }}
+        >{max.toFixed(1) + 'x'}</span>
       </div>
     );
-  }, [trackRef.current]);
+  }, [trackRef.current, value, min, max]);
 
   const TrackSlider = useCallback(() => {
     const track = trackRef.current?.offsetWidth;
@@ -68,12 +74,16 @@ export default function DiscreteSliders({ min, max, step = "0.1", range = 5, val
     }
     const w = track / (max - min) * (value - min);
     return <div className="discrete-sliders__slider" style={{ width: `${w}px` }}></div>
-  }, [trackRef.current, value]);
+  }, [trackRef.current, value, min, max]);
 
   return (
     <div className={`discrete-sliders flex flex-column ${className}`}>
       <input
-        ref={trackRef}
+        ref={(ref) => {
+          if (trackRef.current !== ref) {
+            setTrackRef({ current: ref })
+          }
+        }}
         className={`discrete-sliders__track ${className}`}
         type="range"
         min={min}
