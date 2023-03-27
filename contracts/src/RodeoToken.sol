d40c135be3560c5edb9e79b3ac1c3bf8e406863e
smfnd <smfnd@protonmail.com>
2023-03-27 10:43:33 -0400

    :bug: fix issue

--- contracts/src/RodeoToken.sol
+++ contracts/src/RodeoToken.sol
@@ -10,7 +10,9 @@ contract RodeoToken is ERC20 {
     }
 
     uint256 public weeklySupply; // percentage value
+    uint256 public lastMintTime;
     address public mintAddr;
+    address public owner;
 
     event Mint(address indexed dst, uint256 amt);
     event SetWeeklySupply(address indexed owner, uint256 amt);
@@ -25,6 +27,8 @@ contract RodeoToken is ERC20 {
         uint256 _initialSupply
     ) ERC20(_name, _symbol, _decimals) {
         uint256 whiteListTotalAmt;
+        owner = msg.sender;
+
         for (uint256 i = 0; i < _whitelist.length; i++) {
             whiteListTotalAmt += _whitelist[i].amt;
             _mint(_whitelist[i].usr, _whitelist[i].amt);
@@ -36,7 +40,7 @@ contract RodeoToken is ERC20 {
         require(mintAddr != address(0), "!undefined");
         require(block.timestamp >= lastMintTime + 1 weeks, "Can only mint once per week");
         
-        uint256 amount = totalSupply() * weeklySupply / 100;
+        uint256 amount = totalSupply * weeklySupply / 100;
 
         _mint(mintAddr, amount);
         lastMintTime = block.timestamp;
@@ -45,11 +49,16 @@ contract RodeoToken is ERC20 {
 
     function setWeeklySupply(uint256 _weeklySupply) external onlyOwner {
         weeklySupply = _weeklySupply;
-        emit SetWeeklySupply(owner(), _weeklySupply);
+        emit SetWeeklySupply(owner, _weeklySupply);
     }
 
     function setMintAddr(address _mintAddr) external onlyOwner {
         mintAddr = _mintAddr;
-        emit SetMintAddr(owner(), _mintAddr);
+        emit SetMintAddr(owner, _mintAddr);
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "!unauthorized");
+        _;
     }
 }
