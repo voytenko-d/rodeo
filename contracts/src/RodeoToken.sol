15cdbcf99b24e96aacba9976a9a1778f28e42394
smfnd <smfnd@protonmail.com>
2023-04-02 21:53:03 -0400

    :bug: fix comments

--- contracts/src/RodeoToken.sol
+++ contracts/src/RodeoToken.sol
@@ -7,51 +7,68 @@ import {Util} from "./Util.sol";
 contract Token is ERC20Permit, Util {
     error NoMintAddress();
     error NoEmissionSent();
+    error NotPaused();
+    error NotTimeYet(uint256 waitToMint);
 
-    uint256 public emissionRate; // percentage value
-    uint256 public MAX_BPS = 1e18;
+    uint256 public emissionRate; // percentage value, 1e18 = 100%
 
     uint256 public lastMintTime;
     uint256 public decayPerWeek;
     uint256 public lastAmtPerWeek;
+    uint256 public deadline;
+    uint256 public weeklyPercent;
     address public emissionsRecipient;
 
     event Mint(address indexed dst, uint256 amt);
     event SetEmissions(address indexed owr, uint256 amt, uint256 dec);
     event SetEmissionRecipient(address indexed owr, address emi);
 
-    constructor(uint256 _initialSupply) ERC20Permit("Rodeo", "RODEO", 18) {
+    constructor(uint256 spl) ERC20Permit("Rodeo", "RODEO", 18) {
         exec[msg.sender] = true;
-        _mint(msg.sender, _initialSupply);
+        _mint(msg.sender, spl);
+        paused = true;
     }
 
-    function emissionMint() external {
+    function emissionMint() external live {
         if (emissionsRecipient == address(0)) {
             revert NoMintAddress();
         }
-        uint256 timeElapsed = block.timestamp - lastMintTime;
-        uint256 decayPercent = timeElapsed * decayPerWeek / 604800;
-        lastAmtPerWeek = lastAmtPerWeek * (MAX_BPS - decayPercent) / MAX_BPS;
-        uint amtToMint = timeElapsed * lastAmtPerWeek / 604800;
-
-        uint receivedAmt = balanceOf[emissionsRecipient];
-        _mint(emissionsRecipient, amtToMint);
-        receivedAmt = balanceOf[emissionsRecipient] - receivedAmt;
-        if (receivedAmt != amtToMint) revert NoEmissionSent();
-        lastMintTime = block.timestamp;
-        emit Mint(emissionsRecipient, amtToMint);
+        if (block.timestamp <= deadline) {
+            uint256 timeElapsed = block.timestamp - lastMintTime;
+            uint256 decayPercent = timeElapsed * decayPerWeek / 604800;
+            lastAmtPerWeek = lastAmtPerWeek * (1e18 - decayPercent) / 1e18;
+            uint amtToMint = timeElapsed * lastAmtPerWeek / 604800;
+            _mint(emissionsRecipient, amtToMint);
+            lastMintTime = block.timestamp;
+            emit Mint(emissionsRecipient, amtToMint);
+        } else {
+            if (lastMintTime + 1 weeks > block.timestamp) {
+                revert NotTimeYet(lastMintTime + 1 weeks - block.timestamp);
+            }
+            uint256 amtToMint = totalSupply * weeklyPercent / 1e18;
+            _mint(emissionsRecipient, amtToMint);
+            emit Mint(emissionsRecipient, amtToMint);
+        }
     }
 
-    function setEmissions(uint256 _emissionRate, uint256 _decayPerWeek) external auth {
-        emissionRate = _emissionRate;
+    function setEmissions(uint256 _emissionRate, uint256 _decayPerWeek, uint256 _deadline, uint256 _weeklyPercent) external auth {
+        if (!paused) {
+            revert NotPaused();
+        }
+        lastAmtPerWeek = _emissionRate;
         decayPerWeek = _decayPerWeek;
-        lastAmtPerWeek = totalSupply * emissionRate / MAX_BPS;
-        lastMintTime = block.timestamp;
+        deadline = _deadline;
+        weeklyPercent = _weeklyPercent;
         emit SetEmissions(msg.sender, _emissionRate, _decayPerWeek);
     }
 
     function setEmissionsRecipient(address emi) external auth {
         emissionsRecipient = emi;
         emit SetEmissionRecipient(msg.sender, emi);
+    }
+
+    function activate() external auth {
+        lastMintTime = block.timestamp;
+        paused = false;
     }
 }
--- contracts/src/test/Token.t.sol
+++ contracts/src/test/Token.t.sol
@@ -16,28 +16,38 @@ contract TokenTest is Test {
     }
 
     function testMint() public {
-        vm.expectRevert(Token.NoMintAddress.selector);
-        token.emissionMint();
-
         token.setEmissionsRecipient(address(this));
-        vm.expectEmit(true, true, false, false);
-        emit Mint(address(this), 0);
-        token.emissionMint();        
 
-        // NOTE: emissionRate 1%, decayRate 5%
-        token.setEmissions(1e16, 5e16);
+        // NOTE: emissionRate 1000, decayRate 5%
+        token.setEmissions(1000, 5e16, block.timestamp + 604800*5, 1e16);
+        token.activate();
         vm.warp(block.timestamp + 86400);
 
+        uint balanceBefore = token.balanceOf(address(this));
         // decaypercent: 86400 * 5e16 / 604800 = 7.1418571e15
         // lastAmtPerWeek: 1000 * (1e18-decayPercent) / 1e18 = 992
         // amtToMint: 86400 * lastAmtPerWeek / 604800
         vm.expectEmit(true, true, false, false);
         emit Mint(address(this), 141);
         token.emissionMint();
+        uint balanceAfter = token.balanceOf(address(this));
+        assertEq(balanceAfter - balanceBefore, 141);
 
         vm.warp(block.timestamp + 86400);
+        balanceBefore = token.balanceOf(address(this));
         vm.expectEmit(true, true, true, true);
         emit Mint(address(this), 140);
         token.emissionMint();
+        balanceAfter = token.balanceOf(address(this));
+        assertEq(balanceAfter - balanceBefore, 140);
+
+        vm.warp(block.timestamp + 604800*7);
+        uint tokenToBeMinted = token.totalSupply() / 100;
+        balanceBefore = token.balanceOf(address(this));
+        vm.expectEmit(true, true, false, false);
+        emit Mint(address(this), tokenToBeMinted);
+        token.emissionMint();
+        balanceAfter = token.balanceOf(address(this));
+        assertEq(balanceAfter - balanceBefore, tokenToBeMinted);
     }
 }
